6ea777838d982d942f7da4945b299cef23b9eea9
timislearning <tim.mitzen@gmail.com>
2020-09-24 14:05:31 -0700

    Updates

---
+++ precache-manifest.44a42b8a0fdd09506c51d32088a37127.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6964d1c022c9dd85af3e864e41e54a3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9877e58aa771051f05bb",
+    "url": "/static/css/main.133117f5.chunk.css"
+  },
+  {
+    "revision": "253137cea13d6368d0e5",
+    "url": "/static/js/2.bf658b7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bf658b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9877e58aa771051f05bb",
+    "url": "/static/js/main.3e93272d.chunk.js"
+  },
+  {
+    "revision": "85d367d40f4625aef104",
+    "url": "/static/js/runtime-main.1281186e.js"
+  }
+]);
