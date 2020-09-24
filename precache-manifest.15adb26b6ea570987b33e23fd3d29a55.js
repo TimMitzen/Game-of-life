8a5f6fda9b1b13534ad39f00698697b7f68caba3
timislearning <tim.mitzen@gmail.com>
2020-09-24 14:11:18 -0700

    Updates

---
+++ precache-manifest.15adb26b6ea570987b33e23fd3d29a55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9416f7e503d307d7854a51f8fcf0afe1",
+    "url": "/Game-of-life/index.html"
+  },
+  {
+    "revision": "6f6bca48c7fbad52f447",
+    "url": "/Game-of-life/static/css/main.133117f5.chunk.css"
+  },
+  {
+    "revision": "253137cea13d6368d0e5",
+    "url": "/Game-of-life/static/js/2.bf658b7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Game-of-life/static/js/2.bf658b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f6bca48c7fbad52f447",
+    "url": "/Game-of-life/static/js/main.e858cca0.chunk.js"
+  },
+  {
+    "revision": "5b3d2ea738176774adb5",
+    "url": "/Game-of-life/static/js/runtime-main.eb43cb65.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44a42b8a0fdd09506c51d32088a37127.js"
+  "/Game-of-life/precache-manifest.15adb26b6ea570987b33e23fd3d29a55.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Game-of-life/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
